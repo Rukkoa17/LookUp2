32b031414b9b450584cb57b04bede04253d7b87a
Rukka <dariociorba02@gmail.com>
2026-09-11 19:51:07 +0200

    Implementing moonphase (weatherapi)

--- backend/app.py
+++ backend/app.py
@@ -72,11 +72,45 @@ def weather():
     if weather_key :
         weather_key = weather_key.strip()
 
-    latitude = request.args.get("lat")
-    longitude = request.args.get("lon")
+    wlatitude = request.args.get("lat")
+    wlongitude = request.args.get("lon")
 
-    response = requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={weather_key}&units=metric"
+    mlatitude = request.args.get("latitude")
+    mlongitude = request.args.get("longitude")
+    from_date = request.args.get("from_date")
+    to_date = request.args.get("to_date")
+    time = request.args.get("time")
+    
+    
+    weather_response = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?lat={wlatitude}&lon={wlongitude}&appid={weather_key}&units=metric"
     )
     
-    return jsonify(response.json())
+    moonphase_response = requests.get(
+        f"https://api.astronomyapi.com/api/v2/studio/moon-phase?&latitude={mlatitude}&longitude={mlongitude}&elevation=0&from_date=${from_date}&to_date=${to_date}&time=${time}"
+    )
+
+    return jsonify(weather_response.json())
+
+@app.route("/api/moonphase")
+def moonphase():
+
+    weather_key = os.getenv("WEATHER_API_KEY")
+
+    if not weather_key:
+        return jsonify({"error : Weather API key is missing"}) , 500
+
+    if weather_key :
+        weather_key = weather_key.strip()
+
+    mlatitude = request.args.get("latitude")
+    mlongitude = request.args.get("longitude")
+    from_date = request.args.get("from_date")
+    to_date = request.args.get("to_date")
+    time = request.args.get("time")
+    
+    moonphase_response = requests.get(
+        f"https://api.astronomyapi.com/api/v2/studio/moon-phase?&latitude={mlatitude}&longitude={mlongitude}&elevation=0&from_date=${from_date}&to_date=${to_date}&time=${time}"
+    )
+
+    return jsonify(moonphase_response.json())
